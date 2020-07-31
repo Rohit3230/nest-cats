17c5dc296c87dadb8ac8a850053862d1f723e60e
RSN <rsn3230@gmail.com>
2020-07-31 10:47:04 +0530

    comit codoe for fundamentals topic study

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Its Rohit (A MEAN Stack Developer) From Here.';
+    return 'Hello World!';
   }
 }
--- src/modules/cats/cats.controller.spec.ts
+++ src/modules/cats/cats.controller.spec.ts
@@ -1,18 +1,72 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CatsController } from './cats.controller';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { CatsController } from './cats.controller';
 
-describe('Cats Controller', () => {
-  let controller: CatsController;
+// describe('Cats Controller', () => {
+//   let controller: CatsController;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [CatsController],
+//     }).compile();
+
+//     controller = module.get<CatsController>(CatsController);
+//   });
+
+//   it('should be defined', () => {
+//     expect(controller).toBeDefined();
+//   });
+// });
+
+
+
+// import { CatsController } from './cats.controller';
+// import { CatsService } from './cats.service';
+
+// describe('CatsController', () => {
+//   let catsController: CatsController;
+//   let catsService: CatsService;
+
+//   beforeEach(() => {
+//     catsService = new CatsService();
+//     catsController = new CatsController(catsService);
+//   });
+
+//   describe('findAll', () => {
+//     it('should return an array of cats', async () => {
+//       const result = [];
+//       jest.spyOn(catsService, 'findAll').mockImplementation(() => result);
+
+//       expect(await catsController.findAll()).toBe(result);
+//     });
+//   });
+// });
+
+
+
+import { Test } from '@nestjs/testing';
+import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+describe('CatsController', () => {
+  let catsController: CatsController;
+  let catsService: CatsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CatsController],
-    }).compile();
+    const moduleRef = await Test.createTestingModule({
+        controllers: [CatsController],
+        providers: [CatsService],
+      }).compile();
 
-    controller = module.get<CatsController>(CatsController);
+    catsService = moduleRef.get<CatsService>(CatsService);
+    catsController = moduleRef.get<CatsController>(CatsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = [];
+      jest.spyOn(catsService, 'findAll').mockImplementation(() => result);
+
+      expect(await catsController.findAll()).toBe(result);
+    });
   });
 });
